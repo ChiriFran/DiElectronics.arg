48345f0713ffe50f01ca5c4f75e98eb01fd8cde6
Chiri Francisco <franchiri00@gmail.com>
2024-04-27 20:18:25 -0300

    vs

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,5 +1,6 @@
 const openMenu = document.querySelector("#open-menu");
 const closeMenu = document.querySelector("#close-menu");
+const closeMenuLupa = document.querySelector("#lupaImg");
 const aside = document.querySelector("aside");
 
 openMenu.addEventListener("click", () => {
@@ -8,6 +9,11 @@ openMenu.addEventListener("click", () => {
 });
 
 closeMenu.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+    document.body.classList.remove("overflow-hidden");
+});
+
+closeMenuLupa.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
     document.body.classList.remove("overflow-hidden");
 });
